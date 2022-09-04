5ee1ad31aad98897e0f32f6557f922020dcc7778
이석민 <jshlhs1@gmail.com>
2022-09-04 17:29:26 +0000

    main.py edited online with Bitbucket

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,13 @@
-print('test')
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def main():
+	return {"message": "Helloworld，FastAPI"}
+
+
+if __name__ == '__main__':
+	import uvicorn
+	uvicorn.run(app, host="127.0.0.1", port=8089)
