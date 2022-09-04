a1abe706b9d781a815a4aa74b2baeb2ecd9cc809
ESukmean <esukmean@esukmean.com>
2022-09-05 01:40:16 +0900

    sveltekit 으로 static 사이트 build

--- front/svelte.config.js
+++ front/svelte.config.js
@@ -1,9 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+                adapter: adapter({
+                    pages: 'build',
+                    assets: 'build',
+                    fallback: '200.html',
+                    precompress: false
+                }),
 
 		// Override http methods in the Todo forms
 		methodOverride: {
