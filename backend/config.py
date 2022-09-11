44b396647472617a4a59ade1072bb5c45f76d405
ESukmean <esukmean@esukmean.com>
2022-09-11 18:49:21 +0000

    딸려 들러간 커스텀 설정파일 삭제, 커스텀 설정을 동적으로 불러오게 수정

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,15 @@
 import config_default
+import os
 
 
 class Config(config_default.Config):
-    HTTP = {"port": 8090}
+    pass
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+custom_config_exists = os.path.exists(dir_path + "/config_custom.py")
+
+if custom_config_exists:
+    import config_custom
+
+    Config = config_custom.Config
