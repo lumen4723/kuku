cf6934d78cb39552ee4b92155c1c9123f3450cb5
haminu <you@example.com>
2022-09-17 06:09:15 +0900

    Landing_Page_V1

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,2 +1,54 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="hero is-primary is-halfheight">
+    <div class="hero-body">
+        <div class="container">
+            <div class="columns">
+                <div class="column"
+                style="align-items: center;
+                justify-content: center;
+                display: flex;
+                padding-bottom: 10%;">
+                    <div>
+                        <h1 class="title" style="word-break: keep-all;">
+                            차 한잔과 함께하는 코딩 사이트
+                        </h1>
+                        <h2 class="subtitle"
+                        style="padding-left: 48px;
+                        padding-top: 16px;
+                        line-height:1.4;">
+                            비전공자도 참여할 수 있는
+                            <br>
+                            쉬운 난이도를 통하여
+                            <br>
+                            여러분의 코딩실력을 향상시켜보세요!
+                        </h2>
+                    </div>
+                </div>
+                <div class="column">
+                    <div style="padding-left: 30px; padding-top: 10px">
+                        <img src="https://via.placeholder.com/560x500" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="hero-body">
+    <div class="container">
+        <div class="columns" style="margin: 60px 0px 20px 0px;">
+            <div class="column">
+                <h1 class="title" 
+                style="word-break: keep-all; 
+                padding-left: 15%;">
+                    다양한 알고리즘 트랙
+                </h1>
+            </div>
+        </div>
+        <div class="columns">
+            <div class="column">
+                <div class="box" style="width: 80%; margin: 0 auto; height: 200px">
+                    I'm in a box.
+                  </div>
+            </div>
+        </div>
+    </div>
+</section>
