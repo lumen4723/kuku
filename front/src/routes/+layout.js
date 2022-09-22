cc9220177e524996618bb4a44f86fc2a5767292f
HanDaWoon <bookpia.han@gmail.com>
2022-09-22 08:42:20 +0000

    csr = true

--- front/src/routes/+layout.js
+++ front/src/routes/+layout.js
@@ -1,5 +1,5 @@
 // This can be false if you're using a fallback (i.e. SPA mode)
 // src/routes/+layout.js
-export const csr = false;
+export const csr = true;
 export const ssr = false;
 export const prerender = true;
