a9decf3ead547f7155ab6a77d3e68489b7119ac6
kimjunseo <a@.com>
2022-09-30 22:10:04 +0900

    Revert "qna/+layout내용삭제"
    This reverts commit 5cc5f11da576c9a39fd78f27162363441c9c1688.

--- front/src/routes/board/qna/+layout.svelte
+++ front/src/routes/board/qna/+layout.svelte
@@ -3,6 +3,20 @@
 	import Page from './[page]/+page.svelte';
 </script>
 
+{#if $page.url.pathname === '/board/qna/list/[page]'}
+	<header>
+		<!-- 검색 -->
+		<section class="hero is-small ">
+			<div class="hero-body">
+				<div class="container">
+					<h1 class="title">질문</h1>
+					<h2 class="subtitle">자유롭게 질문할 수 있는 게시판입니다.</h2>
+				</div>
+			</div>
+		</section>
+	</header>
+{/if}
+
 <main class="container">
 	<slot />
 </main>
