240d8224310dcf89b2b5ecd065d2bc5afe9fe4fc
kimjunseo <a@.com>
2022-10-23 23:40:16 +0900

    10월 23일 작업

--- front/src/routes/board/qna/[page]/+page.svelte
+++ front/src/routes/board/qna/[page]/+page.svelte
@@ -1,10 +1,12 @@
 <script>
   import { userIsLogged } from "$lib/user.js";
+  import { onMount } from "svelte";
 
   let currentPage = 1;
   let pageLimit = 10;
   let label = 1;
   const isLogged = userIsLogged();
+  let message = "message";
 
   const getBoardList = async (pageIdx, pageLimit) => {
     const res = await fetch(
@@ -13,19 +15,19 @@
         mode: "cors",
         credentials: "include",
       }
-    );
-    const qnaBoard = await res.json();
-    //const freeBoard = await res.json();
-    if (res.ok) {
-      return qnaBoard;
-      //return freeBoard;
-    } else {
-      throw new Error(qnaBoard);
-      //throw new Error(freeBoard);
-    }
+    )
+      .then((res) => {
+        if (res.ok == false) return Promise.reject(res);
+        {
+          return res.json();
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
 
-  $: boardList = getBoardList(currentPage, pageLimit, label); // 이거 뭐야 파라메터 안맞음
+  $: boardList = getBoardList(currentPage, pageLimit); // 이거 뭐야 파라메터 안맞음
 </script>
 
 <div class="container">
--- front/src/routes/board/qna/article/[id]/+page.svelte
+++ front/src/routes/board/qna/article/[id]/+page.svelte
@@ -1,322 +1,285 @@
 <script>
-  import { page } from "$app/stores";
-  import List from "../../[page]/+page.svelte";
-  import { onMount } from "svelte";
-  import Swal from "sweetalert2";
+	import { page } from "$app/stores";
+	import List from "../../[page]/+page.svelte";
+	// import Header from '$lib/header/HeaderBQC.svelte';
 
-  const getArticle = async (article_id) => {
-    const res = await fetch(
-      `//api.eyo.kr:8081/board/qna/article/${article_id}?article_id=${article_id}`,
-      {
-        mode: "cors",
-        credentials: "include",
-      }
-    );
+	const getArticle = async (article_id) => {
+		const res = await fetch(
+			`//127.0.0.1:8081/board/qna/article/${article_id}?article_id=${article_id}`,
+			{
+				mode: "cors",
+				credentials: "include",
+			}
+		);
 
-    const qnaboard = await res.json();
-    if (res.ok) {
-      return qnaboard;
-    } else {
-      throw new Error(qnaboard);
-    }
-  };
+		const qnaboard = await res.json();
+		if (res.ok) {
+			return qnaboard;
+		} else {
+			throw new Error(qnaboard);
+		}
+	};
 
-  const delArticle = async (article_id) => {
-    const res = await fetch(
-      `//api.eyo.kr:8081/board/qna/article/${article_id}`,
-      {
-        method: "DELETE",
-        mode: "cors",
-        credentials: "include",
-      }
-    ).then((res) => {
-      console.log(res);
-      if (res.ok == false) {
-        return Promise.reject(res);
-      } else {
-        return res.json();
-      }
-    });
-  };
+	let article = getArticle($page.params.id);
 
-  const del = () => {
-    Swal.fire({
-      title: "삭제하시겠습니까?",
-      text: "다시 되돌릴 수 없습니다.",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "RGB(067, 085, 189)",
-      cancelButtonColor: "RGB(219, 224, 255)",
-      confirmButtonText: "삭제",
-      cancelButtonText: "취소",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire("Deleted!", "글이 삭제되었습니다.", "success").then(
-          (result) => {
-            delArticle($page.params.id)
-              .then((res) => {
-                console.log(res);
-              })
-              .catch((err) => {
-                console.log(err);
-                err.text().then((text) => {
-                  console.log(text);
-                });
-              });
-            if (result.isConfirmed) location.href = "/board/free/1";
-          }
-        );
-      }
-    });
-  };
+	const like_qna = async (article_id) => {
+		const res = await fetch(
+			`//127.0.0.1:8081/board/qna/${article_id}/like`,
+			{
+				method: "POST",
+				mode: "cors",
+				credentials: "include",
 
-  let article = getArticle($page.params.id);
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					article_id,
+				}),
+			}
+		);
+		const like = await res.json();
+		if (res.ok) {
+			return like;
+		} else {
+			throw new Error(like);
+		}
+	};
 
-  const like_qna = async (article_id) => {
-    const res = await fetch(
-      `http://api.eyo.kr:8081/board/qna/article/${article_id}/like`,
-      {
-        method: "POST",
-        mode: "cors",
+	//dislike_qna fetch 함수
+	const dislike_qna = async (article_id) => {
+		const res = await fetch(
+			`//127.0.0.1:8081/board/qna/article/${article_id}/dislike`,
+			{
+				method: "POST",
+				mode: "cors",
+				credentials: "include",
 
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          article_id: article_id,
-        }),
-      }
-    );
-    const like = await res.json();
-    if (res.ok) {
-      return like;
-    } else {
-      throw new Error(like);
-    }
-  };
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					article_id,
+				}),
+			}
+		);
+		const dislike = await res.json();
+		if (res.ok) {
+			return dislike;
+		} else {
+			throw new Error(dislike);
+		}
+	};
 
-  const dislike_qna = async (article_id) => {
-    const res = await fetch(
-      `http://api.eyo.kr:8081/board/qna/article/${article_id}/dislike`,
-      {
-        method: "POST",
-        mode: "cors",
+	let isClicked = false;
+	const likeclick = () => {
+		isClicked = !isClicked;
+		if (isClicked) {
+			like_qna($page.params.id);
+		} else {
+			dislike_qna($page.params.id);
+		}
+	};
+	const alt = () => {
+		alert("로그인이 필요합니다.");
+	};
 
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          article_id: article_id,
-        }),
-      }
-    );
-    const dislike = await res.json();
-    if (res.ok) {
-      return dislike;
-    } else {
-      throw new Error(dislike);
-    }
-  };
+	let isClicked2 = false;
+	const likeclick2 = () => {
+		isClicked2 = !isClicked2;
+	};
+	const alt2 = () => {
+		alert("로그인이 필요합니다.");
+	};
 
-  let isClicked = false;
-  const likeclick = () => {
-    isClicked = !isClicked;
-  };
-  const alt = () => {
-    alert("로그인이 필요합니다.");
-  };
-
-  let isClicked2 = false;
-  const likeclick2 = () => {
-    isClicked2 = !isClicked2;
-  };
-  const alt2 = () => {
-    alert("로그인이 필요합니다.");
-  };
-
-  let isLogin = true;
-  onMount(async () => {
-    if (window.localStorage["user.username"] == null) {
-      isLogin = false;
-    }
-  });
+	let isLogin = true;
 </script>
 
 {#await article then article}
-  <header>
-    <div style="padding: 16px">
-      {#if isLogin}
-        <div class="edit" style="float: right; margin-top: 16px">
-          <a href="/board/qna/write/question/{article.article_id}">
-            <button class="button is-rounded is-light"> 수정 </button></a
-          >
-          <button
-            class="button is-rounded is-light"
-            type="submit"
-            on:click={del}
-          >
-            삭제
-          </button>
-        </div>
-      {/if}
-      <div style="float:left;">
-        <span class="is-size-3">{article.title}</span>
-        {#if article.answers == true}
-          <span style="float: inline-end;">
-            <span class="icon is-large ">
-              <i
-                class="fas fa-check-circle fas fa-2x"
-                style="color: greenyellow"
-              />
-            </span>
-          </span>
-        {:else}
-          <span style="float: inline-end;">
-            <span class="icon is-large">
-              <i
-                class="fas fa-check-circle fas fa-2x"
-                style="color: #4A4A4A;"
-              />
-            </span>
-          </span>
-        {/if}
-        <br />
-        <div style="float: left;">
-          <a class="author" href="/" style="color: #4A4A4A;"
-            >{article.username}</a
-          >
-          <span style="color: #DBDBDB;">|</span>
-          {article.created}
-        </div>
-      </div>
-      <div style="clear:both" />
-      <div style="float:left;">
-        <div>
-          <div class="icon is-medium" style="float: left;">
-            <i class="fa-solid fa-tag" />
-          </div>
-          <a href="/">
-            <button
-              class="button is-rounded is-link is-light is-small is-responsive"
-            >
-              태그
-            </button>
-          </a>
-        </div>
-      </div>
-    </div>
-  </header>
+	<header>
+		<div style="padding: 16px">
+			{#if isLogin}
+				<div class="edit" style="float: right; margin-top: 16px">
+					<a href="/"
+						><button class="button is-rounded is-light">
+							수정
+						</button></a
+					><a href="/board/qna/1">
+						<button class="button is-rounded is-light">
+							삭제
+						</button>
+					</a>
+				</div>
+			{/if}
+			<div style="float:left;">
+				<span class="is-size-3">{article.title}</span>
+				{#if article.answers == true}
+					<span style="float: inline-end;">
+						<span class="icon is-large ">
+							<i
+								class="fas fa-check-circle fas fa-2x"
+								style="color: greenyellow"
+							/>
+						</span>
+					</span>
+				{:else}
+					<span style="float: inline-end;">
+						<span class="icon is-large">
+							<i
+								class="fas fa-check-circle fas fa-2x"
+								style="color: #4A4A4A;"
+							/>
+						</span>
+					</span>
+				{/if}
+				<br />
+				<div style="float: left;">
+					<a class="author" href="/" style="color: #4A4A4A;"
+						>{article.username}</a
+					>
+					<span style="color: #DBDBDB;">|</span>
+					{article.created}
+				</div>
+			</div>
+			<div style="clear:both" />
+			<div style="float:left;">
+				<div>
+					<div class="icon is-medium" style="float: left;">
+						<i class="fa-solid fa-tag" />
+					</div>
+					<a href="/">
+						<button
+							class="button is-rounded is-link is-light is-small is-responsive"
+						>
+							태그
+						</button>
+					</a>
+				</div>
+			</div>
+		</div>
+	</header>
 
-  <div style="clear:both" />
+	<div style="clear:both" />
 
-  <hr style="margin:0;" />
+	<hr style="margin:0;" />
 
-  <div class="content">
-    {@html article.content}
-  </div>
-
-  <form
-    style="margin: 0 0 0 100%; width: auto%; text-align: center; float: right"
-    on:submit|preventDefault={isClicked ? like_qna : dislike_qna}
-  >
-    <span class="is-size-3">
-      {#if isLogin}
-        <i
-          class={isClicked ? "fa-solid fa-heart" : "fa-regular fa-heart"}
-          on:click={likeclick}
-        />
-      {:else}
-        <i class="fa-regular fa-heart" on:click={alt} />
-      {/if}
-    </span>
-    <span>추천 {article.like}</span>
-  </form>
+	<div class="content">
+		{@html article.content}
+		<!--이 부분은 질문의 내용 부분입니다. <br />
+	양식에 맞게 질문을 작성해주세요. <br /><br />
+	ex) 여기 링크가 있습니다. <a href="/">기술 블로그</a> <br />
+	,등등-->
+	</div>
+	<form>
+		<div
+			style="margin: 0 0 0 100%; width: auto%; text-align: center; float: right"
+		>
+			<span class="is-size-3">
+				{#if isLogin}
+					<i
+						class={isClicked
+							? "fa-solid fa-heart"
+							: "fa-regular fa-heart"}
+						on:click={likeclick}
+					/>
+				{:else}
+					<i class="fa-regular fa-heart" on:click={alt} />
+				{/if}
+			</span>
+			<span>추천 {article.like}</span>
+		</div>
+	</form>
 {/await}
 
 <hr style="margin-top: 0;" />
 
 <div style="padding: 5px ">
-  <div style="float: left;">
-    <a href="/board/qna/1"
-      ><button class="button is-rounded is-light">목록</button></a
-    >
-  </div>
-  <div style="float: right;">
-    <a href="/board/qna/write/answer"
-      ><button class="button is-rounded is-light">답글 작성</button></a
-    >
-  </div>
+	<div style="float: left;">
+		<a href="/board/qna/1"
+			><button class="button is-rounded is-light">목록</button></a
+		>
+	</div>
+	<div style="float: right;">
+		<a href="/board/qna/write/answer"
+			><button class="button is-rounded is-light">답글 작성</button></a
+		>
+	</div>
 </div>
 <br /><br />
 <div style="padding: 16px">
-  <div style="float: left;">
-    <div class="icon is-medium" style="float: left;">
-      <i class="fa-solid fa-comment" />
-    </div>
-    <span class="is-size-4">답변</span>
-  </div>
-  <div style="clear:both" />
+	<div style="float: left;">
+		<div class="icon is-medium" style="float: left;">
+			<i class="fa-solid fa-comment" />
+		</div>
+		<span class="is-size-4">답변</span>
+	</div>
+	<div style="clear:both" />
 </div>
 
 {#await article}
-  <p class="has-text-centered">Loading in progress...</p>
+	<p class="has-text-centered">Loading in progress...</p>
 {:then article}
-  {#each article["answers"] as answer}
-    <div class="comment">
-      <table class="table container is-fluid comment_table">
-        <tbody>
-          <tr>
-            <td
-              style="text-align: left; width: 100px; border-right: 2px solid #dbdbdb; padding: 10px;"
-            >
-              <a class="comment_author" href="/" style="color: #4A4A4A;"
-                >{answer.author}</a
-              >
-            </td>
-            <td style="width: 900px;">{answer.created}</td>
-            <td style="left: 100%;">
-              {#if isLogin}
-                <button
-                  class="button is-rounded is-link is-light is-small is-responsive"
-                >
-                  수정
-                </button>
-                <button
-                  class="button is-rounded is-link is-light is-small is-responsive"
-                >
-                  삭제
-                </button>
-              {/if}
-            </td>
+	{#each article["answers"] as answer}
+		<div class="comment">
+			<table class="table container is-fluid comment_table">
+				<tbody>
+					<tr>
+						<td
+							style="text-align: left; width: 100px; border-right: 2px solid #dbdbdb; padding: 10px;"
+						>
+							<a
+								class="comment_author"
+								href="/"
+								style="color: #4A4A4A;">{answer.author}</a
+							>
+						</td>
+						<td style="width: 900px;">{answer.created}</td>
+						<td style="left: 100%;">
+							{#if isLogin}
+								<button
+									class="button is-rounded is-link is-light is-small is-responsive"
+								>
+									수정
+								</button>
+								<button
+									class="button is-rounded is-link is-light is-small is-responsive"
+								>
+									삭제
+								</button>
+							{/if}
+						</td>
 
-            <div style="text-align: center; float: right;">
-              <span class="is-size-4">
-                {#if isLogin}
-                  <i
-                    class={isClicked2
-                      ? "fa-solid fa-heart"
-                      : "fa-regular fa-heart"}
-                    on:click={likeclick2}
-                  />
-                {:else}
-                  <i class="fa-regular fa-heart" on:click={alt} />
-                {/if}
-              </span>
-              <span>추천 {answer.like}</span>
-            </div>
-          </tr>
-        </tbody>
-      </table>
+						<div style="text-align: center; float: right;">
+							<span class="is-size-4">
+								{#if isLogin}
+									<i
+										class={isClicked2
+											? "fa-solid fa-heart"
+											: "fa-regular fa-heart"}
+										on:click={likeclick2}
+									/>
+								{:else}
+									<i
+										class="fa-regular fa-heart"
+										on:click={alt}
+									/>
+								{/if}
+							</span>
+							<span>추천 {answer.like}</span>
+						</div>
+					</tr>
+				</tbody>
+			</table>
 
-      <div class="comment_WordStyle">
-        <div class="comment_content">
-          {@html answer.content}<br />
-        </div>
-      </div>
+			<div class="comment_WordStyle">
+				<div class="comment_content">
+					{@html answer.content}<br />
+				</div>
+			</div>
 
-      <hr style="margin: 0;" />
-    </div>
-  {/each}
+			<hr style="margin: 0;" />
+		</div>
+	{/each}
 {/await}
 
 <br /><br />
@@ -336,30 +299,30 @@
 <List />
 
 <style>
-  hr {
-    border: 1px solid #dbdbdb;
-  }
-  span i {
-    position: static;
-    color: rgb(251, 106, 130);
-  }
-  .content {
-    width: 100%;
-    height: 100px;
-    padding: 16px;
-  }
-  .comment table {
-    width: 100%;
-    border: 1px solid #dbdbdb;
-    background-color: rgba(239, 235, 235, 0.805);
-    margin: 0 0 0 0;
-  }
-  .comment_WordStyle {
-    padding: 0 100px 0 100px;
-    height: 100px;
-    border-left: solid 2px #dbdbdb;
-    border-right: solid 2px #dbdbdb;
-    /*		border-bottom-right-radius: 10px;
+	hr {
+		border: 1px solid #dbdbdb;
+	}
+	span i {
+		position: static;
+		color: rgb(251, 106, 130);
+	}
+	.content {
+		width: 100%;
+		height: 100px;
+		padding: 16px;
+	}
+	.comment table {
+		width: 100%;
+		border: 1px solid #dbdbdb;
+		background-color: rgba(239, 235, 235, 0.805);
+		margin: 0 0 0 0;
+	}
+	.comment_WordStyle {
+		padding: 0 100px 0 100px;
+		height: 100px;
+		border-left: solid 2px #dbdbdb;
+		border-right: solid 2px #dbdbdb;
+		/*		border-bottom-right-radius: 10px;
 		border-bottom-left-radius: 10px; */
-  }
+	}
 </style>
