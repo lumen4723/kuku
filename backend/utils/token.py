3c0c85910fd606f46902dad62b075805a21ec21d
ESukmean <esukmean@esukmean.com>
2022-11-14 02:35:37 +0000

    master로 merge 하면서 생긴 conflict 수정

--- backend/utils/token.py
+++ backend/utils/token.py
@@ -3,20 +3,19 @@ from utils.exception import *
 from config_default import Config
 import jwt
 from result import Result, Ok, Err
-<<<<<<< HEAD
-from urllib import parse 
-def token_generator_by_email(email:str) -> Result:
+from urllib import parse
+
+
+def token_generator_by_email(email: str) -> Result:
     try:
-        token_data = {
-            "email": email
-        }
-        token = jwt.encode(token_data,Config.session_key,algorithm="HS256")
+        token_data = {"email": email}
+        token = jwt.encode(token_data, Config.session_key, algorithm="HS256")
         token = parse.quote(token)
         return Ok(token)
     except Exception as e:
         return Err(str(e))
 
-<<<<<<< HEAD
+
 def verify_token(token: str) -> Result:
     try:
         token_data = jwt.decode(token, Config.session_key, algorithms=["HS256"])
---
+++ front/src/routes/study/[course]/write/+page.svelte
@@ -0,0 +1,129 @@
+<script>
+	import { onMount } from "svelte";
+	import { browser } from "$app/env";
+	import { is_empty } from "svelte/internal";
+	import Swal from "sweetalert2";
+	let title = "",
+		content = "";
+	let ckeditorInstance;
+	// let ClassicEditor;
+	onMount(async () => {
+		if (browser)
+			ClassicEditor.create(document.querySelector("#editor"), {
+				simpleUpload: {
+					// The URL that the images are uploaded to.
+					uploadUrl: "//api.eyo.kr:8081/upload/",
+
+					// Enable the XMLHttpRequest.withCredentials property.
+					withCredentials: true,
+				},
+			})
+				.then((editor) => {
+					ckeditorInstance = editor;
+				})
+				.catch((error) => {
+					console.error(error);
+				});
+	});
+
+	const postArticle = () =>
+		fetch(`//api.eyo.kr:8081/board/study/create`, {
+			method: "POST",
+			headers: {
+				Aceept: "application/json",
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				title,
+				content: ckeditorInstance.getData(),
+			}),
+			mode: "cors",
+			credentials: "include",
+		})
+			.then((res) => {
+				if (res.ok == false) return Promise.reject(res);
+				return res.json();
+			})
+			.then((json) => {
+				postResult = JSON.stringify(json);
+			})
+			.catch((err) => {
+				console.log(err);
+				return err;
+			});
+
+	const upload = () => {
+		console.log(title);
+		console.log(
+			JSON.stringify({
+				title,
+				content: ckeditorInstance.getData(),
+			})
+		);
+		postArticle()
+			.then((res) => {
+				console.log(res);
+			})
+			.catch((err) => {
+				console.log(err);
+				err.text().then((text) => {
+					console.log(text);
+				});
+			});
+	};
+	const alt = () => {
+		Swal.fire({
+			title: "제목을 입력해주세요",
+			icon: "error",
+			confirmButtonText: "확인",
+		});
+	};
+</script>
+
+<svelte:head>
+	<script src="/ckeditor.js"></script>
+</svelte:head>
+
+<br />
+<form method="POST" on:submit|preventDefault={upload}>
+	<div class="contents">
+		<input
+			class="input mb-4"
+			type="text"
+			placeholder="제목을 입력해주세요"
+			bind:value={title}
+			required
+		/>
+		<textarea
+			class="textarea"
+			id="editor"
+			placeholder="내용을 입력하세요."
+			required>{content}</textarea
+		>
+		<hr />
+	</div>
+	{#if is_empty(title)}
+		<button class="button is-link" on:click={alt}>완료</button>
+	{:else}
+		<a href="/board/free/1"
+			><button class="button is-link" type="submit" on:click={upload}
+				>완료</button
+			>
+		</a>
+	{/if}
+	<a href="/board/free/1"
+		><button class="button is-link is-light" type="button">취소</button></a
+	>
+</form>
+<br /> <br />
+
+<style>
+	textarea {
+		width: 100%;
+		height: 50em;
+		resize: none;
+	}
+	:global(.ck-editor__editable_inline) {
+		min-height: 400px;
+	}
+</style>
